10a651397e8a6956bb17157912e5cf36fae6e714
J Hautier <jhautier@hotmail.fr>
2021-05-08 18:56:23 +0200

    fix: Change email provider

--- controllers/mailer.js
+++ controllers/mailer.js
@@ -2,32 +2,39 @@ import nodemailer from "nodemailer"
 import user from "../models/user.js"
 import ask from "../models/ask.js"
 import 'dotenv/config.js';
-import nodemailermailguntransport from 'nodemailer-mailgun-transport';
+// import nodemailermailguntransport from 'nodemailer-mailgun-transport';
 
-const auth = {
-  auth: {
-    api_key: 'key-13fd560150b2f10971f19c6dccc1b9f6-2a9a428a-a9d49f54',
-    domain: 'sandbox113eca2185224408a92d493e8ae25c9b.mailgun.org'
+// const auth = {
+//   auth: {
+//     api_key: 'key-13fd560150b2f10971f19c6dccc1b9f6-2a9a428a-a9d49f54',
+//     domain: 'sandbox113eca2185224408a92d493e8ae25c9b.mailgun.org'
 
-  }
-}
+//   }
+// }
   // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport(nodemailermailguntransport(auth));
+
 
 export const sendNotification = async (req, res) => {
-   try{
+  let transporter = nodemailer.createTransport({
+    host: "smtp-relay.sendinblue.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user:"jhautier@hotmail.fr", // generated ethereal user
+      pass:"NBJVvCDL51aqr0O7", // generated ethereal password
+    },
+  });
+  //  try{
  
    const {id} = req.params
    const {userid, hyperlink} = req.body
-   
+  try{
+
    userid.map(async (userid) => {
-    try{
    const foundUser = await user.findById(userid); 
    const {email, username} = foundUser;
    const foundAsk = await ask.findById(id);
    const {whatsthat} = foundAsk;
-
-
    let info = await transporter.sendMail({
     from: '"Whats that" <jhautier@hotmail.fr>', // sender address
     to: email, // list of receivers
@@ -35,17 +42,19 @@ export const sendNotification = async (req, res) => {
     
     html: `<b>Your question about $${whatsthat} has received a new answer! Check it out at <a href=${hyperlink}> ${hyperlink}</b>`, // html body
   });
-  
+  // }catch(error) {res.status(500).json({error:error.message})}
+
   console.log("Message sent: %s", info.messageId);
   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
   // Preview only available when sending through an Ethereal account
   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
   // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+)
+res.send("emails sent successfuly")
   }catch(error) {res.status(500).json({error: error.message})}
-})
-res.send("success")
 
-}catch(error) {res.status(500).json({error:error.message})}
+
 }
 
